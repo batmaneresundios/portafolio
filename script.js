6d5a6801317b43a33d3cc2b070938a7d9507f729
Fernanda <batmaneresundios@gmail.com>
2025-08-31 21:01:47 -0400

    Primer commit del portafolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// Animación de scroll suave para futuras mejoras
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
